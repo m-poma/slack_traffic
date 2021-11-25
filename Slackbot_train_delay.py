0be6921427b36995d41968808a774b7c56aa7f70
mipopoma <pechemn@gmail.com>
2021-11-25 15:10:15 +0900

    2021/11/25の遅延情報jsonに更新

--- Slackbot_train_delay.py
+++ Slackbot_train_delay.py
@@ -3,7 +3,7 @@ import json
 
 # 鉄道遅延情報のjsonサイトからデータを取得
 url =  "https://tetsudo.rti-giken.jp/free/delay.json"
-delay_data = json.load("20210627.json")
+delay_data = json.load("20211125.json")
 
 # WEB_HOOK_URLはSlackの設定で準備で発行したURLを設定。
 WEB_HOOK_URL = "https://hooks.slack.com/services/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"

512a9f2fb07f5b0aa31649aafa22864b94a003f8
mipopoma <pechemn@gmail.com>
2021-11-25 14:55:02 +0900

    slackAPIで遅延情報を確認する

---
+++ Slackbot_train_delay.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+# 鉄道遅延情報のjsonサイトからデータを取得
+url =  "https://tetsudo.rti-giken.jp/free/delay.json"
+delay_data = json.load("20210627.json")
+
+# WEB_HOOK_URLはSlackの設定で準備で発行したURLを設定。
+WEB_HOOK_URL = "https://hooks.slack.com/services/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"
+"
+"""
+requests.post(WEB_HOOK_URL,json.dumps)
+
+#print (delay_data)
+"""
+
+
+print (delay_data)
+print(type(delay_data))
+print (delay_data[1])
